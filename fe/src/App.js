b336868b98db22cd365019a3b6d7011a3bb400e2
sangltv <sangltv@proton.me>
2022-11-30 11:17:43 +0000

    ulod

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,11 +1,22 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Home from './views/Home'
 import Login from './views/Login'
 import Users from './views/Users'
 import {Provider} from './Context'
+import {useBeforeUnload} from 'react-use'
+
 const App = () => {
     const [dt, setDt] = useState({})
+
+    useEffect(()=>{
+        console.log("Load first time")
+    },[])
+
+    useBeforeUnload(()=>{
+        console.log("Before Unload")
+    })
+
     return <Provider value={{dt,setDt}}>
 <Routes>
     <Route path='/' element = {<Home />}/>
