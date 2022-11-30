c33cb18889589f4bca7e822261ffbe9eac71bb55
sangltv <sangltv@proton.me>
2022-11-30 12:02:25 +0000

    get express

--- be/src/index.js
+++ be/src/index.js
@@ -1,6 +1,17 @@
-const http = require('http')
-const server = http.createServer((request, respond)=>{
-    respond.write("Helloo Oto")
-    respond.end()
+const express = require('express')
+const app = express()
+const users = [
+    {id:0,
+    username:"admin",
+    password:"admin"},
+     {id:1,
+    username:"long",
+    password:"longpass"}
+]
+app.get("/",(request, respond)=>{
+    respond.send("HOME")
 })
-server.listen(3000)
+app.get("/users",(request, respond)=>{
+    respond.json(users)
+})
+app.listen(3000)
